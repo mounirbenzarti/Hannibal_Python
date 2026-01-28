831e672900e63dceeb8b72638dbbf4ce79ad7718
Mounir Benzarti <mounirbenzarti@gmail.com>
2026-01-28 10:52:23 +0100

    Update page title in Streamlit configuration
    Demo présentation Roger

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from streamlit_folium import st_folium
 
 # Configuration de la page
 st.set_page_config(
-    page_title="L'Odyssée d'Hannibal Barca",
+    page_title="L'Odyssée d'Hannibal",
     page_icon="🐘",
     layout="wide"
 )
